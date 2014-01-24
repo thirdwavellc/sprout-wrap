316fd7d78ecd667731fcd0e94539fdbb4f557b0a
Adam Krone <krone.adam@gmail.com>
2014-01-24 10:23:21 -0600

    Updates package installer

--- installer/build.sh
+++ installer/build.sh
@@ -2,5 +2,6 @@ pkgbuild --identifier "com.thirdwave.sprout-wrap"\
 	--version "1.0.0"\
 	--root "./payload"\
 	--install-location "/tmp/sprout-wrap/"\
+	--scripts "./scripts/"\
 	--sign "Developer ID Installer: Adam Krone"\
 	SproutWrap.pkg
